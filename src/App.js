bf7563cae8cfd1369ab90412db9a406912f670e6
jutello-lycsa <jutello@lartirigoyen.com>
2026-01-29 11:20:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PRIMER CAMBIO:
+        Versión 0.0.6
+      </h1>
+    </div>
+  );
+}
+
+export default App;
